f231f8265147f025d464f0fc31ac6bca2d2e7e74
Konstantin Munteanu <konstantin@munteanu.de>
2023-03-18 15:01:52 +0100

    remove reqwest

--- src/client/mfa.rs
+++ src/client/mfa.rs
@@ -1,18 +1,18 @@
 use serde::Deserialize;
 use serde_json::json;
+use attohttpc::StatusCode;
 
 use super::session::Session;
 use super::Result;
 
 pub fn request(session: &Session, auth_option: &str) -> Result<MfaInfo> {
     let body = json!({ "authOption": auth_option });
-    let req = session
-        .post("verification/api/identity_verification/request_authorization_code")
+    let req = session.post("verification/api/identity_verification/request_authorization_code")
         .json(&body)?;
     let res = req.send()?;
     match res.status() {
-        reqwest::StatusCode::OK => res.json::<MfaInfo>().map_err(super::Error::from),
-        reqwest::StatusCode::BAD_REQUEST => res.json::<MfaInfo>().map_err(super::Error::from),
+        StatusCode::OK => res.json::<MfaInfo>().map_err(super::Error::from),
+        StatusCode::BAD_REQUEST => res.json::<MfaInfo>().map_err(super::Error::from),
         _ => Err(res.into()),
     }
 }
@@ -24,8 +24,8 @@ pub fn submit(session: &Session, auth_option: &str, code: &str) -> Result<MfaInf
         .json(&body)?;
     let res = req.send()?;
     match res.status() {
-        reqwest::StatusCode::OK => res.json::<MfaInfo>().map_err(super::Error::from),
-        reqwest::StatusCode::BAD_REQUEST => res.json::<MfaInfo>().map_err(super::Error::from),
+        StatusCode::OK => res.json::<MfaInfo>().map_err(super::Error::from),
+        StatusCode::BAD_REQUEST => res.json::<MfaInfo>().map_err(super::Error::from),
         _ => Err(res.into()),
     }
 }
