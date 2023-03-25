cc9ff702d962e98932734e0f7103a3653936cf62
Konstantin Munteanu <konstantin@munteanu.de>
2023-03-25 23:16:47 +0100

    Add logging crates

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ mod client;
 mod commands;
 mod persistence;
 
+use std::fs::File;
+
 use clap::Parser;
 use commands::Commands;
+use directories::ProjectDirs;
+use log::LevelFilter;
+use simplelog::{WriteLogger, Config};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -13,6 +18,11 @@ struct Cli {
 }
 
 fn main() {
+    if let Some(proj_dirs) = ProjectDirs::from("rs", "",  "rippling-cli") {
+        let dir = proj_dirs.config_dir();
+        let _ = WriteLogger::init(LevelFilter::Info, Config::default(), File::create(dir.join("default.log")).unwrap());
+    }
+
     let cli = Cli::parse();
 
     commands::execute(&cli.command)
