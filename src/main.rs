7452a0869d724dcca668ce3bf63391f813914b36
Konstantin Munteanu <637499+mkon@users.noreply.github.com>
2023-03-27 00:28:18 +0200

    Add Authenticator support (#19)

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod client;
 mod commands;
 mod persistence;
 
+use std::fs::File;
+
 use clap::Parser;
 use commands::Commands;
+use directories::ProjectDirs;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -13,7 +16,16 @@ struct Cli {
 }
 
 fn main() {
+    init_logging();
     let cli = Cli::parse();
-
     commands::execute(&cli.command)
+}
+
+fn init_logging() {
+    if let Some(proj_dirs) = ProjectDirs::from("rs", "", "rippling-cli") {
+        let dir = proj_dirs.config_dir();
+        let file = File::create(dir.join("default.log")).unwrap();
+        let pipe = env_logger::Target::Pipe(Box::new(file));
+        env_logger::Builder::from_default_env().target(pipe).init();
+    }
 }
