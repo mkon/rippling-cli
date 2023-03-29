e3b825d476bdbdfd0f3ef2c99908172a01b89141
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2023-03-29 08:57:24 +0200

    Pass token during MFA to skip user input prompt.

--- src/commands/mfa.rs
+++ src/commands/mfa.rs
@@ -7,10 +7,16 @@ pub struct Command {
     pub facility: Facility,
 }
 
+#[derive(Debug, Parser)]
+pub struct Token {
+    #[structopt(long)]
+    token: Option<String>,
+}
+
 #[derive(Debug, Subcommand)]
 pub enum Facility {
     /// For use with a token generator like Google Authenticator
-    Token,
+    Token(Token),
     /// Enter the code which will be sent to your email address
     Email,
     /// Enter the code which will be sent to your Phone (SMS)
@@ -18,8 +24,8 @@ pub enum Facility {
 }
 
 pub fn execute(cmd: &Command) {
-    match cmd.facility {
-        Facility::Token => token_flow(),
+    match &cmd.facility {
+        Facility::Token(token) => token_flow(token.token.as_deref()),
         Facility::Email => request_flow("EMAIL"),
         Facility::Mobile => request_flow("PHONE_TEXT"),
     }
@@ -32,8 +38,17 @@ fn request_flow(facility: &str) {
     super::wrap_in_spinner(|| client::mfa::submit(session, facility, &code), |r| r.message);
 }
 
-fn token_flow() {
-    let code = super::ask_user_input("Enter the code");
+fn token_flow(token: Option<&str>) {
+    let input_code: Option<String>;
+    let code: &str;
+
+    if let Some(t) = token {
+        code = t;
+    } else {
+        input_code = Some(super::ask_user_input("Enter the code"));
+        code = input_code.as_ref().unwrap();
+    }
+
     super::wrap_in_spinner(
         || client::mfa::token(&super::get_session(), &code),
         |r| if r { "Code valid".into() } else { "Code invalid".into() },
