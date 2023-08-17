a90320ff8a66a53a6c480c6ca10de52e46d48afa
Konstantin Munteanu <konstantin@munteanu.de>
2023-08-17 22:13:18 +0200

    Create config dir when missing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@ mod commands;
 mod persistence;
 
 use std::{
-    fs::File,
+    fs::{self, File},
     io::IsTerminal,
     sync::atomic::{AtomicBool, Ordering},
 };
@@ -30,6 +30,7 @@ fn main() {
 fn init_logging() {
     if let Some(proj_dirs) = ProjectDirs::from("rs", "", "rippling-cli") {
         let dir = proj_dirs.config_dir();
+        fs::create_dir_all(dir).unwrap();
         let file = File::create(dir.join("default.log")).unwrap();
         let pipe = env_logger::Target::Pipe(Box::new(file));
         env_logger::Builder::from_default_env().target(pipe).init();
